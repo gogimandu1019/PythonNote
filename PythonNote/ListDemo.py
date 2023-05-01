3a9695a446eeb3391266cc8088c0fab40e0eae54
gogimandu1019 <indigosky94@naver.com>
2023-05-01 21:18:57 +0900

    20230501 list

---
+++ PythonNote/ListDemo.py
@@ -0,0 +1,15 @@
+colors = ["red", "green", "blue"]
+
+print(colors[0])
+print(colors[1])
+print(colors[2])
+print(colors[-1])
+print(colors[-2])
+print(colors[-3])
+
+print(len(colors))
+
+print(colors[1:])
+
+for color in colors:
+	print(color)
